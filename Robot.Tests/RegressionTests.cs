5c47e87cf766351182c1622c03b3180efcdfa50c
Pawel Klimcyzk <pawel@klimczyk.pl>
2013-11-26 08:39:26 +0100

    ScreenCast 1

---
+++ Robot.Tests/RegressionTests.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="RegressionTests.cs" project="Robot.Tests" date="2013-11-25 22:59">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+namespace Robot.Tests
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RegressionTests
+    {
+        [Test]
+        [Sequential]
+        public void Tests([Values(@"1
+1 1
+FRF", @"1
+1 1
+RFFLFRB", @"1
+-1 -1
+BRB", @"3
+1 1
+2 2
+3 3
+FRFFLFFRF", @"1
+1 1
+FRFBF")] string consoleInput, [Values("1\r\n", "1\r\n", "1\r\n", "3\r\n", "1\r\n")] string expectedValue)
+        {
+            //arrange
+            StringBuilder builder = new StringBuilder();
+            TextWriter writer = new StringWriter(builder);
+            TextReader reader = new StringReader(consoleInput);
+
+            Console.SetIn(reader);
+            Console.SetOut(writer);
+
+            //act
+            Program.Main(null);
+
+            //assert
+            Assert.That(builder.ToString(), Is.EqualTo(expectedValue));
+        }
+    }
+}
